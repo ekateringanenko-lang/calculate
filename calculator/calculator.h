50f87449a27e6de645c762c5e094112390fc84b5
ekateringanenko-lang <ekaterin.ganenko@gmail.com>
2026-02-02 11:36:23 +0300

    Update calculator.h

--- calculator/calculator.h
+++ calculator/calculator.h
@@ -1,1 +1,14 @@
 // В этом файле объявления функций.
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+#include <string>
+
+// Псевдоним типа
+using Number = double;
+
+// Сигнатуры функций
+bool ReadNumber(Number& result);
+bool RunCalculatorCycle();
+
+#endif  // CALCULATOR_H
